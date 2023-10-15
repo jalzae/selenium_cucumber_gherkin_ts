ab5fabea57e65f4c30c2fed1bf927e1b3de1ee90
JalzAe <jalzae@gmail.com>
2023-10-15 09:24:04 +0700

    Test with firefox

---
+++ index.js
@@ -0,0 +1,29 @@
+const webdriver = require("selenium-webdriver");
+const By = webdriver.By;
+const until = webdriver.until;
+
+let driver_fx = new webdriver.Builder().forBrowser("firefox").build();
+
+searchTest(driver_fx);
+
+function searchTest(driver) {
+  driver.get("http://www.google.com");
+  driver.findElement(By.name("q")).sendKeys("webdriver");
+
+  driver.sleep(3000).then(() => {
+    driver.findElement(By.name("q")).sendKeys(webdriver.Key.ENTER);
+  });
+
+
+  driver.sleep(5000).then(() => {
+    driver.getTitle().then((title) => {
+      console.log('Title : ', title)
+      if (title === "webdriver - Penelusuran Google") {
+        console.log("Test passed");
+      } else {
+        console.log("Test failed");
+      }
+      driver.quit();
+    });
+  });
+}
